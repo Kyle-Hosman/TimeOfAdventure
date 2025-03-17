e70312ac272f048f67c71fac53834c615de27d31
Kyle Hosman <khosman@msudenver.edu>
2025-03-16 19:40:50 -0600

    initial setup of dialogue panel

---
+++ Assets/ChatPanelManager.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+
+public class ChatPanelManager : MonoBehaviour
+{
+
+    public GameObject chatPanel;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        chatPanel.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            ToggleChatPanel();
+        }
+    }
+
+    public void ToggleChatPanel()
+    {
+        chatPanel.SetActive(!chatPanel.activeSelf);
+    }
+    public void CloseChatPanel()
+    {
+        chatPanel.SetActive(false);
+    }
+}
